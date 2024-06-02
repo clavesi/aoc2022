735db5bee9e00b7853dd065ace312c9e4cd10547
clavesi <34937562+clavesi@users.noreply.github.com>
2024-06-02 16:46:44 -0500

    day1a

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::eyre::Context;
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    let input = include_str!("input.txt");
+    let input_replace = input.replace("\r\n", "\n");
+    let groups = input_replace.split("\n\n");
+
+    let mut max = 0;
+    for group in groups {
+        let mut sum = 0;
+        for line in group.lines() {
+            let value = line.parse::<u64>()?;
+            sum += value;
+        }
+        println!("GROUP END WITH {sum}");
+
+        if sum > max {
+            max = sum;
+        }
+    }
+    println!("MAX: {max}");
+
+    Ok(())
+}
+
+fn read_input() -> color_eyre::Result<String> {
+    let input = std::fs::read_to_string("src/input.txt").wrap_err("reading src/input.txt")?;
+    return Ok(input);
 }
